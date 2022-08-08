7f16255e40594f84ac67fae8a2c6aa46016f8ebf
ashinsabu <ashin.sabu3@gmail.com>
2022-08-08 21:30:58 +0530

    Assets and image loader

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import generateJoke from "./generateJoke";
 import './styles/main.scss'
+import saffron from './assets/saffron.png'
 
+const saffronImg = document.getElementById('saffronImg')
+saffronImg.src = saffron
 console.log(generateJoke());
-console.log(1);
